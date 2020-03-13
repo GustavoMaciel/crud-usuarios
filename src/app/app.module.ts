ced9580f20004f71947edba1a5a439c08a61e800
AmandaAzevedo <amanda.azevedo@dcx.ufpb.br>
2020-03-12 23:31:14 -0300

    criei componente empresa e seu módulo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EmpresaComponent } from './empresa/empresa.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    EmpresaComponent,
   ],
   imports: [
     BrowserModule,
