cb940ecfb60dbfb6ac4d698e429f824e4d870d14
emersondantas <emerson.ruan@dce.ufpb.br>
2020-03-23 16:51:13 -0300

    Adicionado rotas para criar e editar empresa

--- src/app/empresa/empresa-routing.module.ts
+++ src/app/empresa/empresa-routing.module.ts
@@ -1,8 +1,21 @@
 import { Routes, RouterModule } from "@angular/router";
 import { NgModule } from '@angular/core';
+import { EmpresaCriarComponent } from './empresa-criar/empresa-criar.component';
+import { EmpresaEditarComponent } from './empresa-editar/empresa-editar.component';
 
 const routes: Routes = [
-
+  {
+    path: '',
+    component: EmpresaCriarComponent,
+  },
+  {
+      path: "create",
+      component: EmpresaCriarComponent,
+  },
+  {
+      path: "editar/:id",
+      component: EmpresaEditarComponent,
+  }
 ]
 
 @NgModule({
