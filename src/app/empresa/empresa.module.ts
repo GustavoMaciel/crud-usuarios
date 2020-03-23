8f572c01071fd5b605ef21381479694ed4536240
AmandaAzevedo <amanda.azevedo@dcx.ufpb.br>
2020-03-23 18:06:58 -0300

    criado componente listar

--- src/app/empresa/empresa.module.ts
+++ src/app/empresa/empresa.module.ts
@@ -4,11 +4,12 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { EmpresaRoutingModule } from './empresa-routing.module';
 import { EmpresaCriarComponent } from './empresa-criar/empresa-criar.component';
 import { EmpresaEditarComponent } from './empresa-editar/empresa-editar.component';
+import { EmpresaListarComponent } from './empresa-listar/empresa-listar.component';
 
 
 
 @NgModule({
-  declarations: [EmpresaCriarComponent, EmpresaEditarComponent],
+  declarations: [EmpresaCriarComponent, EmpresaEditarComponent, EmpresaListarComponent],
   imports: [
     CommonModule,
     FormsModule,
